054756f9c38632d74c709956844d99b117f74ae5
Aldoleon22 <leonaldo2608@gmail.com>
2024-07-15 11:44:31 +0300

    3 commit

--- src/components/inscription/Inscription.jsx
+++ src/components/inscription/Inscription.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './Inscription.scss';
 import { FaLock, FaRegEyeSlash, FaEnvelope, FaRegUser } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 function Register() {
   const [nom, setNom] = useState('');
+  const navigate =useNavigate(); 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [cpassword, setCpassword] = useState('');
@@ -13,37 +15,22 @@ function Register() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (password !== cpassword) {
-      setError('Les mots de passe ne correspondent pas');
-      return;
-    }
-
     try {
       const response = await axios.post('http://localhost:8000/api/register', {
         name: nom,
         email,
         password,
       });
-
+      navigate('/home');
       console.log('Inscription réussie:', response.data);
       // Redirection ou autre logique après inscription réussie
 
     } catch (error) {
-      if (error.response) {
-        // La requête a été faite et le serveur a répondu avec un statut code qui n'est pas dans les 2xx
-        setError(`Erreur lors de l'inscription: ${error.response.data.message || error.response.data}`);
-        console.error('Erreur lors de l\'inscription:', error.response.data);
-      } else if (error.request) {
-        // La requête a été faite mais aucune réponse n'a été reçue
-        setError('Aucune réponse du serveur');
-        console.error('Requête:', error.request);
-      } else {
-        // Quelque chose s'est passé en configurant la requête qui a déclenché une erreur
-        setError('Erreur lors de l\'inscription');
-        console.error('Erreur:', error.message);
-      }
+      setError('Erreur lors de l\'inscription');
+      console.error('Erreur lors de l\'inscription:', error);
     }
   };
+
   return (
     <div className='register'>
       <div className='form'>
